5aa0614a123a41ce0273aa5574aa57d663306fda
xiaosong <1562631196@qq.com>
2018-01-18 22:38:44 +0800

    添加bind组件用来学习数据绑定的

--- 1.依赖注入/src/app/app.module.ts
+++ 1.依赖注入/src/app/app.module.ts
@@ -8,31 +8,33 @@ import { ProductService } from './shared/product.service';
 import { Product2Component } from './product2/product2.component';
 import { LoggerService } from './shared/logger.service';
 import { AntherProductService } from './shared/anther-product.service';
+import { BindComponent } from './bind/bind.component';
 
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		Product1Component,
-		Product2Component
-	],
-	imports: [
-		BrowserModule
-	],
-	providers: [{
-    provide: ProductService,
-    useFactory : (logger:LoggerService,isDev)=>{
-      if(isDev){
-        return new ProductService(logger);
-      }else{
-        return new AntherProductService(logger);
-      }
-    },
-    deps:[LoggerService,"IS_DEV_ENV"]
-  },LoggerService,
-  {
-    provide:"IS_DEV_ENV", useValue: false
-  }],
-	bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        Product1Component,
+        Product2Component,
+        BindComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [{
+        provide: ProductService,
+        useFactory: (logger: LoggerService, isDev) => {
+            if (isDev) {
+                return new ProductService(logger);
+            } else {
+                return new AntherProductService(logger);
+            }
+        },
+        deps: [LoggerService, "IS_DEV_ENV"]
+    }, LoggerService,
+    {
+        provide: "IS_DEV_ENV", useValue: false
+    }],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ 1.依赖注入/src/app/bind/bind.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-bind',
+    templateUrl: './bind.component.html',
+    styleUrls: ['./bind.component.css']
+})
+export class BindComponent implements OnInit {
+
+    constructor() { }
+
+    doOnclick(event:any){
+        console.log(event);
+    }
+    ngOnInit() {
+
+    }
+
+}
