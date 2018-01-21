45fc6b18137b2cafbc7c556ad6fa04874666ad9d
xiaosong <1562631196@qq.com>
2018-01-21 13:00:46 +0800

    认识响应式编程

--- auction/src/app/response/response.component.ts
+++ auction/src/app/response/response.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-response',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponseComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    Observable.from([1,2,3,4])
+    .filter( e => e%2 === 0)
+    .map( e => e*e)
+    .subscribe(
+        e => console.log(e),
+        err => console.error(err),
+        () => console.log("is end ")
+    )
+   }
 
   ngOnInit() {
   }
