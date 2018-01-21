3a8c13d738075d50f68cafec3054374c1348d95c
xiaosong <1562631196@qq.com>
2018-01-21 13:20:09 +0800

    去掉form标签

--- auction/src/app/response/response.component.ts
+++ auction/src/app/response/response.component.ts
@@ -2,24 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-response',
-  templateUrl: './response.component.html',
-  styleUrls: ['./response.component.css']
+    selector: 'app-response',
+    templateUrl: './response.component.html',
+    styleUrls: ['./response.component.css']
 })
 export class ResponseComponent implements OnInit {
 
-  constructor() {
-    Observable.from([1,2,3,4])
-    .filter( e => e%2 === 0)
-    .map( e => e*e)
-    .subscribe(
-        e => console.log(e),
-        err => console.error(err),
-        () => console.log("is end ")
-    )
-   }
-
-  ngOnInit() {
-  }
+    constructor() {
+        Observable.from([1, 2, 3, 4])
+            .filter(e => e % 2 === 0)
+            .map(e => e * e)
+            .subscribe(
+            e => console.log(e),
+            err => console.error(err),
+            () => console.log("is end ")
+            )
+    }
+    onKey(value:string) {
+        console.log(value);
+    }
+    ngOnInit() {
+    }
 
 }
