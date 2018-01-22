f0b8c1676dfcb348f98ff1e58f40245543273d16
xiaosong <1562631196@qq.com>
2018-01-22 21:51:05 +0800

    实现响应式编程，定使用自定义管道过滤数据

--- auction/src/app/pipe/filter.pipe.ts
+++ auction/src/app/pipe/filter.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+    name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+    transform(list: any[], filterField: string, keyword: string): any {
+        if(!filterField || !keyword){
+            return list;
+        }
+        return list.filter(
+            item =>{
+                let filedValue = item[filterField];
+                return filedValue.indexOf(keyword) >= 0;
+            }
+        );
+    }
 
 }
